e34e00465994e3f88b99c379f1eb32406a09cda7
Okwuone Ogechi Louisa <louisaoge@gmail.com>
2019-12-05 10:17:52 +0100

    bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,49 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+List<BottomNavigationBarItem> _items;
+String _value ='';
+int _index = 0;
+
+  @override
+  void initState() {
+    _items = new List();
+    _items.add(new BottomNavigationBarItem(icon: new Icon(Icons.people), title: new Text('people')));
+    _items.add(new BottomNavigationBarItem(icon: new Icon(Icons.weekend), title: new Text('weekend')));
+    _items.add(new BottomNavigationBarItem(icon: new Icon(Icons.message), title: new Text('message')));
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Name here'),
       ),
-      drawer: new Drawer(
-        child: new Container(
-          padding: new EdgeInsets.all(32.0),
-          child: new Column(
-            children: <Widget>[
-              new Text('Hello World'),
-              new RaisedButton(onPressed: ()=> Navigator.pop(context), child: new Text('click me'),)
-            ],
-          ),
-        ),
-      ),
+
+
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Column(
           children: <Widget>[
-            new Text('Add Widgets Here')
+            new Text(_value)
           ],
         ),
       ),
+      bottomNavigationBar: new BottomNavigationBar(
+          items: _items,
+          fixedColor: Colors.green,
+          backgroundColor: Colors.grey,
+          currentIndex: _index,
+          onTap: (int item){
+            setState(() {
+              _index = item;
+              _value = "Current state = ${_index.toString()}";
+            });
+          },)
+
     );
   }
+
+
 }
